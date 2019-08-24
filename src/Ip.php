0ca00149bc22bd80551589a6d1c5ff36e39e53c4
quid5 <pph@olstudio.co>
2019-08-24 22:45:37 +0000

    Apply fixes from StyleCI

--- src/Ip.php
+++ src/Ip.php
@@ -1,5 +1,12 @@
 <?php
 declare(strict_types=1);
+
+/*
+ * This file is part of the Quid 5 package | https://quid5.com
+ * (c) Pierre-Philippe Emond <emondpph@gmail.com>
+ * License: https://github.com/quid5/base/blob/master/LICENSE
+ */
+
 namespace Quid\Base;
 
 // ip
