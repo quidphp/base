25c10a77536f9e618b75e43f86cac502bf78a2fc
QuidPHP <info@quidphp.com>
2019-09-05 09:40:31 +0000

    Apply fixes from StyleCI

--- test/Classe.php
+++ test/Classe.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 /*
  * This file is part of the QuidPHP package.
  * Website: https://quidphp.com
- * License: https://github.com/quidphp/test/blob/master/LICENSE
+ * License: https://github.com/quidphp/base/blob/master/LICENSE
  */
 
 namespace Quid\Test\Base
