68ec8905df930bed91819ff14f5af199ffd61166
QuidPHP <info@quidphp.com>
2019-11-21 15:17:37 +0000

    Apply fixes from StyleCI

--- test/Dir.php
+++ test/Dir.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 /*
  * This file is part of the QuidPHP package.
+ * Author: Pierre-Philippe Emond <emondpph@gmail.com>
  * Website: https://quidphp.com
  * License: https://github.com/quidphp/base/blob/master/LICENSE
+ * Readme: https://github.com/quidphp/base/blob/master/README.md
  */
 
 namespace Quid\Test\Base;
