5f8b437ff049b7f2dc4ffb93bc814e3ede1fc4ce
QuidPHP <info@quidphp.com>
2020-06-25 02:37:28 +0000

    Apply fixes from StyleCI

--- src/Lang/Fr.php
+++ src/Lang/Fr.php
@@ -2,11 +2,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the QuidPHP package.
+ * This file is part of the QuidPHP package <https://quidphp.com>
  * Author: Pierre-Philippe Emond <emondpph@gmail.com>
- * Website: https://quidphp.com
  * License: https://github.com/quidphp/base/blob/master/LICENSE
- * Readme: https://github.com/quidphp/base/blob/master/README.md
  */
 
 namespace Quid\Base\Lang;
